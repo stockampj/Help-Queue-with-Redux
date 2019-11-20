5cdd875a29589af7092cffb33221121662532da4
Student <example@example.com>
2019-11-20 08:02:12 -0800

    add moment to track our creation/wait time

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -10,10 +10,12 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          formattedWaitTime={ticket.formattedWaitTime}
           key={ticket.id}/>
       )}
     </div>
   );
+
 }
 
 TicketList.propTypes = {
